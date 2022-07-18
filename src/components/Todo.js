16f6da8864e1d605672a9dd8a68891aa01fe2c90
Meka Seba <mekasebameka@gmail.com>
2022-07-17 18:11:37 -0700

    can you check by the trash button isnt working?

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Todo = ({ text, todo, todos, setTodos }) => {
+    const deleteHandler = () => {
+        setTodos(todos.filter(el => el.id !== todo.id))
+    }
+    return(
+        <div className="todo">
+            <li className="todo-item">{text}</li>
+            <button className="complete-btn">Done</button>
+            <button onClick={deleteHandler} className="trash-btn">Trash</button>
+        </div>
+    );
+}
+
+export default Todo;
