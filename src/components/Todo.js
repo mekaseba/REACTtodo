eb0c33f810f3b30932e91cbb2b4a5bfdfec79075
Meka Seba <mekasebameka@gmail.com>
2022-07-17 18:51:33 -0700

    update

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,8 +2,18 @@ import React from "react";
 
 const Todo = ({ text, todo, todos, setTodos }) => {
     const deleteHandler = () => {
-        setTodos(todos.filter(el => el.id !== todo.id))
-    }
+        setTodos(todos.filter(el => el.id !== todo.id));
+    };
+    const completeHandler = () => {
+        setTodos(todos.map(item))=> {
+          if(item.id === todo.id){
+            return {
+                ...item, completed: !item.completed
+            }
+          }
+          return item;
+        })
+    };
     return(
         <div className="todo">
             <li className="todo-item">{text}</li>
